d1ee1a737a04fbd711891de5d09fd99505be2412
Arno Leruste <recroX91@hotmail.com>
2024-10-29 22:21:33 +0100

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
 import { API_ENDPOINT } from '../environments/endpoint.token';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  // providers: [
-  //   { provide: API_ENDPOINT, useValue: environment.apiUrl } // Mettez ici l'URL de votre API
-  // ],
+  providers: [
+    { provide: API_ENDPOINT, useValue: environment.apiUrl } // Mettez ici l'URL de votre API
+  ],
   
 })
 export class AppComponent {
-  title = 'PHILOBM';
+  title = 'PHILOBMFront';
 }
